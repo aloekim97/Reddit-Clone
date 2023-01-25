00870ef7c03bc88c08dc151a320e7f8b3eace526
Alex Kim <aloekim97@gmail.com>
2023-01-25 09:49:26 -0800

    fixes?

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from .api.auth_routes import auth_routes
 from .seeds import seed_commands
 from .config import Config
 from .api.community_routes import community
+from .api.post_routes import post
 
 app = Flask(__name__, static_folder='../react-app/build', static_url_path='/')
 
@@ -29,8 +30,8 @@ app.cli.add_command(seed_commands)
 app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
-app.register_blueprint(community, url_prefix='/api/comm')
-app.register_blueprint(auth_routes, url_prefix='/api/auth')
+app.register_blueprint(community, url_prefix='/api/community')
+app.register_blueprint(post, url_prefix='/api/post')
 
 db.init_app(app)
 Migrate(app, db)
