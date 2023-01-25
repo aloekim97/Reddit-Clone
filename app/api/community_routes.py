fadbfde804ff9bd085edb168e3d2b22fa59776f6
Alex Kim <aloekim97@gmail.com>
2023-01-25 10:54:38 -0800

    fix

--- app/api/community_routes.py
+++ app/api/community_routes.py
@@ -46,26 +46,26 @@ def create_community():
     else:
         return {'errors': validation_errors_to_error_messages(form.errors)}, 400
 
-#update community
-@community.route("/<int:community_id>", methods = ['PUT'])
+# update community
+@community.route("/community/<int:community_id>", methods = ["PUT"])
 @login_required
-def one_community(community_id):
+def edit_community(community_id):
+    comms = [comm.to_dict() for comm in Community.query.filter(community_id)]
     form = CommunityForm()
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
-        comm = Community.query.filter(Community.id == community_id)
         
-        if Community.owner_id == current_user.id:
-            comm.name = form.data['name'],
-            comm.community_img = form.data['community_img'],
-            comm.background_img = form.data['background_img'],
-            comm.description = form.data['description']
+        comms.name = form.data['name'],
+        comms.community_img = form.data['community_img'],
+        comms.background_img = form.data['background_img'],
+        comms.description = form.data['description']
 
         db.session.commit()
-        return {"Community": comm.to_dict()}
+        return {"Community": comms.to_dict()}
 
     else:
         return {'errors': validation_errors_to_error_messages(form.errors)}, 400
+
 
 
 #delte community
@@ -78,5 +78,5 @@ def del_community(community_id):
         db.session.delete(community)
         db.session.commit()
 
-    return {"community": "Community was deleted"}
+    return {"Community": "Community was deleted"}
 
