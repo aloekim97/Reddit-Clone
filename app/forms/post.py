a5c650e82a045bbb4d9fa0f41233f2990b66ea57
Alex Kim <aloekim97@gmail.com>
2023-01-25 09:20:44 -0800

    fixes

--- app/forms/post.py
+++ app/forms/post.py
@@ -2,7 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import IntegerField, StringField
 from wtforms.validators import DataRequired, ValidationError
 from app.models import Post
-from login_form import current_user
 
 class PostForm(FlaskForm):
     user_id = IntegerField(db.String, validators=[DataRequired()]),
