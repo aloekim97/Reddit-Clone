a2d655c492f2172fb4fc03477897181fee25a8d0
Alex Kim <aloekim97@gmail.com>
2023-02-03 00:47:42 -0800

    all seeds

--- app/seeds/comment.py
+++ app/seeds/comment.py
@@ -5,10 +5,38 @@ from datetime import datetime
 # Adds a demo user, you can add other users here if you want
 def seed_comments():
     comments = [
-        Comment(user_id=4, post_id=1, comment="Me too!", created_at=datetime.now()),
-        Comment(user_id=3, post_id=2, comment="Me too!", created_at=datetime.now()),
-        Comment(user_id=2, post_id=4, comment="Me too!", created_at=datetime.now()),
-        Comment(user_id=1, post_id=4, comment="Me too!", created_at=datetime.now()),
+        Comment(user_id=1, post_id=1, comment="same", created_at=datetime.now()),
+        Comment(user_id=2, post_id=2, comment="same", created_at=datetime.now()),
+        Comment(user_id=3, post_id=3, comment="same", created_at=datetime.now()),
+        Comment(user_id=4, post_id=4, comment="same", created_at=datetime.now()),
+        Comment(user_id=1, post_id=5, comment="same", created_at=datetime.now()),
+        Comment(user_id=2, post_id=6, comment="same", created_at=datetime.now()),
+        Comment(user_id=3, post_id=7, comment="same", created_at=datetime.now()),
+        Comment(user_id=4, post_id=8, comment="same", created_at=datetime.now()),
+        Comment(user_id=1, post_id=9, comment="same", created_at=datetime.now()),
+        Comment(user_id=2, post_id=10, comment="same", created_at=datetime.now()),
+        Comment(user_id=1, post_id=11, comment="same", created_at=datetime.now()),
+        Comment(user_id=2, post_id=12, comment="same", created_at=datetime.now()),
+        Comment(user_id=3, post_id=13, comment="same", created_at=datetime.now()),
+        Comment(user_id=4, post_id=14, comment="same", created_at=datetime.now()),
+        Comment(user_id=1, post_id=15, comment="same", created_at=datetime.now()),
+        Comment(user_id=2, post_id=16, comment="same", created_at=datetime.now()),
+        Comment(user_id=3, post_id=17, comment="same", created_at=datetime.now()),
+        Comment(user_id=4, post_id=18, comment="same", created_at=datetime.now()),
+        Comment(user_id=1, post_id=19, comment="same", created_at=datetime.now()),
+        Comment(user_id=2, post_id=20, comment="same", created_at=datetime.now()),
+        Comment(user_id=1, post_id=21, comment="same", created_at=datetime.now()),
+        Comment(user_id=2, post_id=22, comment="same", created_at=datetime.now()),
+        Comment(user_id=3, post_id=23, comment="same", created_at=datetime.now()),
+        Comment(user_id=4, post_id=24, comment="same", created_at=datetime.now()),
+        Comment(user_id=1, post_id=25, comment="same", created_at=datetime.now()),
+        Comment(user_id=2, post_id=26, comment="same", created_at=datetime.now()),
+        Comment(user_id=3, post_id=27, comment="same", created_at=datetime.now()),
+        Comment(user_id=4, post_id=28, comment="same", created_at=datetime.now()),
+        Comment(user_id=1, post_id=29, comment="same", created_at=datetime.now()),
+        Comment(user_id=2, post_id=30, comment="same", created_at=datetime.now()),
+
+
     ]  
     for comment in comments:
         db.session.add(comment)
