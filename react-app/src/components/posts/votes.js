86c712910581edd2ccfd55d6de8e1c94dd97882a
Alex Kim <aloekim97@gmail.com>
2023-04-28 20:37:20 -0700

    edis

--- react-app/src/components/posts/votes.js
+++ react-app/src/components/posts/votes.js
@@ -13,7 +13,7 @@ export default function Votes(post) {
     const userVotes = Object.values(allVotes).filter(v => v.user === user)
     const postVotes = Object.values(userVotes).filter(v => v.post_id === postId)
     const userVote = postVotes[0]?.vote
-    const [total, setTotal] = useState(totVote)
+    // const [total, setTotal] = useState(totVote)
     
 
     const upClick = async (e) => {
@@ -78,29 +78,30 @@ export default function Votes(post) {
         e.preventDefault()
     }
 
+
     return(
         <div className="upholder" onClick={theback}>
             {userVote === 1 ?
                 <div className='upvotes'>
                     <div style={{color: "orange"}} onClick={zero}><i class="fa-solid fa-up-long" ></i></div>
-                    <div className='votenum'>{total}</div>
+                    <div className='votenum'>{totVote}</div>
                     <div style={{color: "white"}} onClick={editDown}><i class="fa-solid fa-down-long" ></i></div> 
                 </div> : 
                 userVote === -1 ?
                     <div className='upvotes'>
                     <div style={{color: "white"}} onClick={editUp}><i class="fa-solid fa-up-long" ></i></div>
-                    <div className='votenum'>{total}</div>
+                    <div className='votenum'>{totVote}</div>
                     <div style={{color: "blue"}} onClick={zero}><i class="fa-solid fa-down-long" ></i></div> 
                 </div> : 
                 userVote === 0 ?
                     <div className='upvotes'>
                     <div style={{color: "white"}} onClick={editUp}><i class="fa-solid fa-up-long"></i></div>
-                    <div className='votenum'>{total}</div>
+                    <div className='votenum'>{totVote}</div>
                     <div style={{color: "white"}} onClick={editDown}><i class="fa-solid fa-down-long" ></i></div> 
                 </div> :
                     <div className='upvotes'>
                     <div style={{color: "white"}} onClick={upClick}><i class="fa-solid fa-up-long" ></i></div>
-                    <div className='votenum'>{total}</div>
+                    <div className='votenum'>{totVote}</div>
                     <div style={{color: "white"}} onClick={downClick}><i class="fa-solid fa-down-long"></i></div> 
                 </div>}
         </div>
